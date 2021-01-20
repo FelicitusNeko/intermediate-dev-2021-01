b085d4ac149b28640a667b3a4bec6874e2863bf1
Anonymous <anonymous@example.com>
2021-01-20 17:08:22 -0400

    web: create RepoDisplay object

--- web/src/objects/App.tsx
+++ web/src/objects/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import '../css/App.css';
 import logo from '../assets/slice.png';
+import { RepoDisplay } from './RepoDisplay';
 
 export function App() {
   return (
     <div className="App">
       <img src={logo} alt="silverorange" />
       <br />
-      <span>Hello World</span>
+      <RepoDisplay />
     </div>
   );
 }
---
+++ web/src/objects/RepoDisplay.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RepoDisplay() {
+  return <>Hello World</>;
+}
+
+// const RepoDisplay: React.FC = () => {
+//   return <>Hello World</>;
+// };
+
+export { RepoDisplay };
