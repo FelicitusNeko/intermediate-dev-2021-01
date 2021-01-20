da89d14f51d203b465445ebda5fcc5d6ea2eb974
Anonymous <anonymous@example.com>
2021-01-20 16:49:39 -0400

    refactor: restructure project also add silverorange logo

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import { App } from './objects/App';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
--- web/src/objects/App.tsx
+++ web/src/objects/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-import './App.css';
+import '../css/App.css';
+import logo from '../assets/slice.png';
 
 export function App() {
   return (
     <div className="App">
+      <img src={logo} alt="silverorange" />
+      <br />
       <span>Hello World</span>
     </div>
   );
