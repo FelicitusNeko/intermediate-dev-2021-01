913ca001409e4cd90f0a5c47a97a0236db4acd51
Anonymous <anonymous@example.com>
2021-01-20 17:37:05 -0400

    web: retrieve data from API

--- web/src/objects/RepoDisplay.tsx
+++ web/src/objects/RepoDisplay.tsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Repo } from '../typings/Repo';
+
+let loading = false;
 
 function RepoDisplay() {
-  return <>Hello World</>;
+  const [repos, setRepos] = useState<Repo[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const getRepos = async () => {
+    // Prevent multiple requests from happening at the same time
+    // Also prevent repos from being retrieved if they already exist (clear them out first)
+    if (loading || repos) {
+      return;
+    }
+    loading = true;
+
+    // eslint-disable-next-line no-console
+    console.debug('getRepos called');
+    setRepos(null);
+    setError(null);
+
+    try {
+      const repoRequest = await axios.get<Repo[]>(
+        'http://localhost:4000/repos'
+      );
+      setRepos(repoRequest.data);
+    } catch (e) {
+      setError(e);
+    } finally {
+      loading = false;
+    }
+  };
+
+  useEffect(() => {
+    // Only initiate loading if there is no data to display
+    if (!repos && !error && !loading) {
+      getRepos();
+    }
+  });
+
+  if (error) {
+    return <>Error: {error}</>;
+  } else if (repos) {
+    return <>There are {repos.length} repos in the request</>;
+  } else {
+    return <>Loading</>;
+  }
 }
 
 // const RepoDisplay: React.FC = () => {
